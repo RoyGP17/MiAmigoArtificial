675a92ffbbb8a89261848e9d14c9d698488cac18
gomez <gomez@BRAYAM>
2022-05-29 23:12:04 -0500

    Pruebita

--- src/main/java/miamigoartificial/MiAmigoArtificial.java
+++ src/main/java/miamigoartificial/MiAmigoArtificial.java
@@ -7,5 +7,6 @@ public class MiAmigoArtificial {
         JOptionPane.showMessageDialog(null, "¡Hola!, Bienvenido: " + a + "\nSara");
         
         System.out.println("Hello World!");
+        System.out.println("Hello, Sara");
     }
 }
