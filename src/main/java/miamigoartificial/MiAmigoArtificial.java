a1e037007198e0490ae689050025ceaba6be1dd1
gomez <gomez@BRAYAM>
2022-05-29 20:48:32 -0500

    Un proyecto que fue relaizado por un grupo de estudiantes de la universidad con el tema MiAmigoArtificial.java.

---
+++ src/main/java/miamigoartificial/MiAmigoArtificial.java
@@ -0,0 +1,11 @@
+package miamigoartificial;
+import javax.swing.JOptionPane;
+public class MiAmigoArtificial {
+
+    public static void main(String[] args) {
+        String a ="Yo soy tu amigo artificial";
+        JOptionPane.showMessageDialog(null, "¡Hola!, Bienvenido: " + a + "\nSara");
+        
+        System.out.println("Hello World!");
+    }
+}
