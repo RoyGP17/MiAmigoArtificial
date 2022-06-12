4c96a96866dcd598cbc944a026f81553c1752141
gomez <gomez@BRAYAM>
2022-06-11 20:52:48 -0500

    "Avance del nombre del usuario"

--- src/main/java/miamigoartificial/MiAmigoArtificial.java
+++ src/main/java/miamigoartificial/MiAmigoArtificial.java
@@ -1,11 +1,19 @@
 package miamigoartificial;
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 public class MiAmigoArtificial {
+    final static Scanner entrada = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String a ="Yo soy tu amigo artificial";
+        String a = "Yo soy tu amigo artificial";
         JOptionPane.showMessageDialog(null, "¡Hola!, Bienvenido: " + a + "\nJarvis");
         
-        System.out.println("Hello World!");
+        recordarNombre();
+    }
+    
+    public static void recordarNombre(){
+        System.out.println("Dime como te llamas");
+        String nombre = entrada.nextLine();
+        System.out.println("Que gran nombre tienes " + nombre + ".");
     }
 }
